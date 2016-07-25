45d8e6317a53ffaad66dff1694515d14f186f9d8
Rushabh Shah <rushabhshah011@outlook.com>
2016-07-25 17:24:58 +0530

    fbuser name fetch

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ const actions = {
 	  https.get('https://graph.facebook.com/v2.6/933900786739383?access_token=EAAENS5edtgwBALkc4d6beZAKSqjUlzHCZAuUf8jPQ5ZAvUQdwsbHL1GdlKpdLwzZCsfuUxnaZAZARwfRAnImDS5ZCjShSTPh74h0vQApuVZBIAt4BXHONxV7lLm03sJeMTpvpfjvDYEw8ZCsCucscOihQGWJsfOX1VcStTOivftXcpwZDZD', (res) => {
 	res.on('data', function (chunk) {
 		console.log('BODY: ' + chunk);
-		
+		console.log('BODY: ' + JSON.parse(chunk));
 		context.fbuname = "Rushabh";
 		  cb(context);
 	});
